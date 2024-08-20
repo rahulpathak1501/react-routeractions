0224a698f94f5177a6e015dd7b0490d2169d7146
rahulpathak1501 <rahul150101130015@gmail.com>
2024-08-20 15:23:16 +0530

    eleted few files and added one new file

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,6 @@
 import { Navigate, Outlet, createBrowserRouter } from "react-router-dom";
-import { postsListRoutes } from "./components/Posts";
-import { usersListRoutes } from "./components/Users";
 import { todosRoutes } from "./components/Todos";
-import { postRoutes } from "./components/Post";
 import Navbar from "./components/Navbar";
-import { userRoutes } from "./components/User";
 
 export const router = createBrowserRouter([
   {
@@ -12,26 +8,7 @@ export const router = createBrowserRouter([
     children: [
       {
         errorElement: <h1>Error: Page not found</h1>,
-        children: [
-          { path: "*", element: <h1>404 - Not Found</h1> },
-          {
-            path: "/posts",
-            children: [
-              { index: true, ...postsListRoutes },
-              { path: ":postId", ...postRoutes },
-            ],
-          },
-
-          {
-            path: "/users",
-            children: [
-              { index: true, ...usersListRoutes },
-              { path: ":userId", ...userRoutes },
-            ],
-          },
-
-          { path: "/todos", ...todosRoutes },
-        ],
+        children: [{ path: "/todos", ...todosRoutes }],
       },
     ],
   },
